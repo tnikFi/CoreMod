d611ca52be599990ea8b9ce461b2074fa8d6eb44
tnikFi <95296580+tnikFi@users.noreply.github.com>
2023-09-21 21:41:08 +0300

    Add db context

---
+++ src/Infrastructure/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Contexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
---
+++ src/Infrastructure/Data/Design/ApplicationDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Data.Design;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        // Get the configuration base path by the Web project's location
+        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "Web");
+
+        // Get the db connection string from the Web project's appsettings.json
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Configure the DbContext
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
