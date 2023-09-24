1b08d9fc435a5594be5c3eed7b6960a10672477c
tnikFi <95296580+tnikFi@users.noreply.github.com>
2023-09-24 17:03:20 +0300

    Add RelatedCase reference migration

---
+++ src/Infrastructure/Data/Migrations/20230924140057_AddRelatedCaseReferenceToModeration.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelatedCaseReferenceToModeration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PardonedCaseNumber",
+                table: "Moderations",
+                newName: "RelatedCaseCaseNumber");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "RelatedCaseGuildId",
+                table: "Moderations",
+                type: "decimal(20,0)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moderations_RelatedCaseGuildId_RelatedCaseCaseNumber",
+                table: "Moderations",
+                columns: new[] { "RelatedCaseGuildId", "RelatedCaseCaseNumber" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Moderations_Moderations_RelatedCaseGuildId_RelatedCaseCaseNumber",
+                table: "Moderations",
+                columns: new[] { "RelatedCaseGuildId", "RelatedCaseCaseNumber" },
+                principalTable: "Moderations",
+                principalColumns: new[] { "GuildId", "CaseNumber" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Moderations_Moderations_RelatedCaseGuildId_RelatedCaseCaseNumber",
+                table: "Moderations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Moderations_RelatedCaseGuildId_RelatedCaseCaseNumber",
+                table: "Moderations");
+
+            migrationBuilder.DropColumn(
+                name: "RelatedCaseGuildId",
+                table: "Moderations");
+
+            migrationBuilder.RenameColumn(
+                name: "RelatedCaseCaseNumber",
+                table: "Moderations",
+                newName: "PardonedCaseNumber");
+        }
+    }
+}
