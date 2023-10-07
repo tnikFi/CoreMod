1d189acb8e3c0667998e80eedf505d93d68ed501
tnikFi <95296580+tnikFi@users.noreply.github.com>
2023-10-07 19:35:53 +0300

    Remove a message from not found error page

--- src/ReactUi/src/pages/NotFound.tsx
+++ src/ReactUi/src/pages/NotFound.tsx
@@ -10,7 +10,6 @@ const messages = [
   'Page not found.',
   'Move along, nothing to see here.',
   'Not all those who wander are lost.',
-  "I'm running out of messages to put here.",
   'If you got here by using the site normally, please raise an issue on GitHub.',
   'Keep looking, you will find it eventually.',
 ]
