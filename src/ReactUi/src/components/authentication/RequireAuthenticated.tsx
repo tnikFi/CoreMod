d6a3ee87fa615fce94ca52200edc5659731e9072
tnikFi <95296580+tnikFi@users.noreply.github.com>
2023-10-07 18:02:15 +0300

    Add RequireAuthenticated component

---
+++ src/ReactUi/src/components/authentication/RequireAuthenticated.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { AuthContext } from 'react-oauth2-code-pkce'
+import AuthenticatedComponent from './AuthenticatedComponent'
+import UnauthenticatedComponent from './UnauthenticatedComponent'
+import PageView from '../PageView'
+import { Button } from '@mui/material'
+
+const RequireAuthenticated: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { login } = React.useContext(AuthContext)
+
+  return (
+    <>
+      <AuthenticatedComponent>{children}</AuthenticatedComponent>
+      <UnauthenticatedComponent>
+        <PageView>
+          <h1>Forbidden</h1>
+          <p style={{ fontSize: 24 }}>You are not logged in.</p>
+          <Button onClick={() => login()}>Login</Button>
+        </PageView>
+      </UnauthenticatedComponent>
+    </>
+  )
+}
+
+export default RequireAuthenticated
