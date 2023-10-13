d20851e22a63d99b130921581bc6ec6ec1f01d65
tnikFi <95296580+tnikFi@users.noreply.github.com>
2023-10-13 14:24:15 +0300

    Refactor moderations to separate component

---
+++ src/ReactUi/src/components/panel/MyModerations.tsx
@@ -0,0 +1,68 @@
+import { Paper, Box, Typography, Button } from '@mui/material'
+import { DataGrid, GridColDef, GridValueFormatterParams } from '@mui/x-data-grid'
+import React from 'react'
+import { ModerationDto } from '../../api'
+
+export interface MyModerationsProps {
+  moderations: ModerationDto[]
+  loading: boolean
+  onRefresh: () => void
+}
+
+/**
+ * Formats a date time string to a human readable format using the system locale and timezone.
+ * The date string is expected to be in UTC.
+ */
+const dateTimeFormatter = (params: GridValueFormatterParams) => {
+  // Parse the date and apply the system timezone offset
+  const date = new Date(params.value as string)
+  date.setMinutes(date.getMinutes() - date.getTimezoneOffset())
+  return date.toLocaleString()
+}
+
+/**
+ * Columns for the moderation data grid.
+ */
+const moderationColumns: GridColDef[] = [
+  { field: 'createdAt', headerName: 'Created At', width: 200, valueFormatter: dateTimeFormatter },
+  { field: 'type', headerName: 'Type', width: 200 },
+  { field: 'reason', headerName: 'Reason', minWidth: 200, flex: 1 },
+  {
+    field: 'expiresAt',
+    headerName: 'Expires At',
+    width: 200,
+    valueFormatter: (params) => (params.value ? dateTimeFormatter(params) : 'Never'),
+  },
+]
+
+const MyModerations: React.FC<MyModerationsProps> = ({
+  moderations,
+  loading: moderationsLoading,
+  onRefresh: getModerations,
+}) => {
+  return (
+    <Paper sx={{ p: 2, height: 500, display: 'flex', flexDirection: 'column' }}>
+      <Box
+        sx={{
+          marginBottom: 2,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h6">My Moderations</Typography>
+        <Button variant="outlined" disabled={moderationsLoading} onClick={getModerations}>
+          Refresh
+        </Button>
+      </Box>
+      <DataGrid
+        columns={moderationColumns}
+        rows={moderations}
+        autoHeight={true}
+        loading={moderationsLoading}
+      />
+    </Paper>
+  )
+}
+
+export default MyModerations
