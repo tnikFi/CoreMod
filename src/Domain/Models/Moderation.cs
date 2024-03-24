2da4ee513ce37bcf856181f1510c8987d1a779e4
tnikFi <95296580+tnikFi@users.noreply.github.com>
2024-03-24 18:12:33 +0200

    Add optional column for Hangfire job id to moderations table

--- src/Domain/Models/Moderation.cs
+++ src/Domain/Models/Moderation.cs
@@ -10,12 +10,14 @@ namespace Domain.Models;
 [PrimaryKey(nameof(Id))]
 public class Moderation
 {
+    [NotMapped] private DateTime? _expiresAt;
+
     /// <summary>
     ///     Id of the moderation action
     /// </summary>
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    
+
     /// <summary>
     ///     ID of the guild where the moderation action was performed
     /// </summary>
@@ -37,9 +39,6 @@ public class Moderation
     [Required]
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 
-    [NotMapped]
-    private DateTime? _expiresAt;
-    
     /// <summary>
     ///     Expiration time of the moderation action. Can only be set if the moderation action type allows temporary
     ///     moderation actions.
@@ -47,8 +46,8 @@ public class Moderation
     public DateTime? ExpiresAt
     {
         get => _expiresAt;
-        // Only allow setting the expiration date if the moderation action type can be temporary
-        set => _expiresAt = EnumUtils.HasAttribute<CanBeTemporaryAttribute>(Type) ? value : null;
+        // Only allow setting the expiration date if the moderation action type can be temporary and the is active
+        set => _expiresAt = EnumUtils.HasAttribute<CanBeTemporaryAttribute>(Type) && Active ? value : _expiresAt;
     }
 
     /// <summary>
@@ -111,4 +110,10 @@ public class Moderation
     /// </summary>
     [Required]
     public ModerationType Type { get; set; }
+
+    /// <summary>
+    ///     Job ID of the scheduled task to unban the user after the ban expires.
+    ///     Should be null if the moderation action is not a ban or if the ban is permanent.
+    /// </summary>
+    public string? JobId { get; set; }
 }
--- src/Infrastructure/Data/Migrations/20240324154356_AddJobIdColumnToModerations.Designer.cs
+++ src/Infrastructure/Data/Migrations/20240324154356_AddJobIdColumnToModerations.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240324154356_AddJobIdColumnToModerations")]
+    partial class AddJobIdColumnToModerations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
